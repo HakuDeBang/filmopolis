e34eb54af00bbe7d29265f67786aa7651ff3d3e9
HakuDeBang <mielotr.dev@gmail.com>
2023-03-22 11:32:27 +0100

    update carousel.php

--- contents/include/carousel.php
+++ contents/include/carousel.php
@@ -3,7 +3,7 @@
     <div class="relative overflow-hidden rounded-xl mx-10 h-80 sm:h-96 md:h-144 lg:h-208 my-10">
         <!-- Item 1 -->
         <div class="hidden duration-15000 ease-in-out" data-carousel-item>
-            <img src="assets/imgfilms//alice_darling/gallery/1.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            <img src="assets/img/films/alice_darling/gallery/1.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
         <!-- Item 2 -->
         <div class="hidden duration-15000 ease-in-out" data-carousel-item>
