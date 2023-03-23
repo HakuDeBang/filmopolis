27008ca1ede3412b2bd840221b30675eac033724
HakuDeBang <mielotr.dev@gmail.com>
2023-03-23 08:59:58 +0100

    adding bottom to grid card

--- contents/include/grid_card_homepage.php
+++ contents/include/grid_card_homepage.php
@@ -1,4 +1,4 @@
-<div class="card-group place-items-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 mx-10">
+<div class="card-group place-items-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 mx-10 mb-10">
     <!-- ITEM 1 -->
     <div class="w-42 sm:w-44 md:w-54">
         <a href="#">
