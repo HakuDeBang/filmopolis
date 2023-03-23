42049496ea719150dec69b3019c95ef2d3b71c7c
HakuDeBang <mielotr.dev@gmail.com>
2023-03-23 09:58:10 +0100

    update prochainement

--- contents/include/prochainement.php
+++ contents/include/prochainement.php
@@ -1,4 +1,4 @@
-<div class="flex flex-col items-center justify-center my-10 md:">
+<div class="flex flex-col items-center justify-center my-10">
     <h2 class="text-antiflashWhite bg-cardinal font-medium rounded-lg text-md px-10 py-1 text-center">Prochainement sur Filmopolis</h2>
 </div>
 <div class="card-group place-items-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 mx-10 mb-10">
