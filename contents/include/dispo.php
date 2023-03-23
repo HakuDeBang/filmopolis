71000954b638451e92ada9992c74a3273568c1af
HakuDeBang <mielotr.dev@gmail.com>
2023-03-23 09:29:48 +0100

    update dispo

--- contents/include/dispo.php
+++ contents/include/dispo.php
@@ -1,6 +1,9 @@
+<!-- BANNER DISPO -->
 <div class="flex flex-col items-center justify-center my-10">
     <h2 class="text-antiflashWhite bg-cardinal font-medium rounded-lg text-md px-10 py-1 text-center">Déjà disponible</h2>
 </div>
+
+<!-- GRID CARD -->
 <div class="card-group place-items-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 mx-10 mb-10">
     <!-- ITEM 1 -->
     <div class="w-42 sm:w-44 md:w-54">
@@ -38,43 +41,45 @@
             <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
         </a>
     </div>
-    <!-- ITEM 1 -->
+    <!-- ITEM 7 -->
     <div class="w-42 sm:w-44 md:w-54">
         <a href="#">
             <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
         </a>
     </div>
-    <!-- ITEM 2 -->    
+    <!-- ITEM 8 -->    
     <div class="w-42 sm:w-44 md:w-54">
         <a href="#">
             <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
         </a>
     </div>
-    <!-- ITEM 3 -->    
+    <!-- ITEM 9 -->    
     <div class="w-42 sm:w-44 md:w-54">
         <a href="#">
             <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
         </a>
     </div>
-    <!-- ITEM 4 -->    
+    <!-- ITEM 10 -->    
     <div class="w-42 sm:w-44 md:w-54">
         <a href="#">
             <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
         </a>
     </div>
-    <!-- ITEM 5 -->    
+    <!-- ITEM 11 -->    
     <div class="w-42 sm:w-44 md:w-54">
         <a href="#">
             <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
         </a>
     </div>
-    <!-- ITEM 6 -->    
+    <!-- ITEM 12 -->    
     <div class="w-42 sm:w-44 md:w-54">
         <a href="#">
             <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
         </a>
     </div>
 </div>
+
+<!-- PAGINATION DISPO -->
 <ul class="flex items-center justify-center -space-x-px mb-10">
     <li>
       <a href="#" class="block px-3 py-2 ml-0 leading-tight text-antiflashWhite bg-spaceCadet border border-cardinal rounded-l-lg dark:text-gray-400">
