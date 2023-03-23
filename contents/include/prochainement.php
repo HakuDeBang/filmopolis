6f5b9856ca3dfdcb96fec61bae8bd3869e7b4148
HakuDeBang <mielotr.dev@gmail.com>
2023-03-23 09:23:50 +0100

    update prochainement

--- contents/include/prochainement.php
+++ contents/include/prochainement.php
@@ -1,7 +1,43 @@
-<div class="flex flex-col items-center justify-center my-10">
+<div class="flex flex-col items-center justify-center my-10 md:">
     <h2 class="text-antiflashWhite bg-cardinal font-medium rounded-lg text-md px-10 py-1 text-center">Prochainement sur Filmopolis</h2>
 </div>
 <div class="card-group place-items-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 mx-10 mb-10">
+    <!-- ITEM 1 -->
+    <div class="w-42 sm:w-44 md:w-54">
+        <a href="#">
+            <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
+        </a>
+    </div>
+    <!-- ITEM 2 -->    
+    <div class="w-42 sm:w-44 md:w-54">
+        <a href="#">
+            <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
+        </a>
+    </div>
+    <!-- ITEM 3 -->    
+    <div class="w-42 sm:w-44 md:w-54">
+        <a href="#">
+            <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
+        </a>
+    </div>
+    <!-- ITEM 4 -->    
+    <div class="w-42 sm:w-44 md:w-54">
+        <a href="#">
+            <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
+        </a>
+    </div>
+    <!-- ITEM 5 -->    
+    <div class="w-42 sm:w-44 md:w-54">
+        <a href="#">
+            <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
+        </a>
+    </div>
+    <!-- ITEM 6 -->    
+    <div class="w-42 sm:w-44 md:w-54">
+        <a href="#">
+            <img class="rounded-lg" src="../../../FILMOPOLIS/assets/img/films/alice_darling/affiche_film.jpg" alt="" />
+        </a>
+    </div>
     <!-- ITEM 1 -->
     <div class="w-42 sm:w-44 md:w-54">
         <a href="#">
