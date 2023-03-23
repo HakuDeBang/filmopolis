3c022eb892c4520fa6bdef73e7f608a191826146
HakuDeBang <mielotr.dev@gmail.com>
2023-03-23 23:03:01 +0100

    adding config pdo in inscription.php, method='post' form_signin.php

--- contents/include/form_signin.php
+++ contents/include/form_signin.php
@@ -6,29 +6,29 @@
         <div class="flex flex-col items-center justify-center mb-2.5">
             <h2 class="text-antiflashWhite bg-cardinal font-bold rounded-lg text-md px-10 py-1 text-center uppercase">Inscription</h2>
         </div>
-        <form class="border border-2 border-cardinal rounded-xl flex flex-col justify-center px-3 py-3 mt-2.5 backdrop-blur-sm">
+        <form class="border border-2 border-cardinal rounded-xl flex flex-col justify-center px-3 py-3 mt-2.5 backdrop-blur-sm" action="" method="post">
             <div class="xl:flex xl:flex-row xl:space-x-3">
                 <div class="mb-3">
-                    <input type="text" id="nom" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Nom" name="nom" required>
+                    <input type="text" id="nom" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Nom" name="nom_users" required>
                 </div>
                 <div class="mb-3">
-                    <input type="text" id="prenom" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Prenom" name="prenom" required>
+                    <input type="text" id="prenom" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Prenom" name="prenom_users" required>
                 </div>
             </div>
             <div class="xl:flex xl:flex-row xl:space-x-3">
                 <div class="mb-3">
-                    <input type="email" id="email" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Adresse email" name="email" required>
+                    <input type="email" id="email" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Adresse email" name="email_users" required>
                 </div>
                 <div class="mb-3">
-                    <input type="email" id="confEmail" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Confirmez votre adresse email" name="conf_email" required>
+                    <input type="email" id="confEmail" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Confirmez votre adresse email" name="conf_email_users" required>
                 </div>
             </div>
             <div class="xl:flex xl:flex-row xl:space-x-3">
                 <div class="mb-3">
-                    <input type="password" id="motdepasse" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Votre mot de passe" name="password" required></input>
+                    <input type="password" id="motdepasse" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Votre mot de passe" name="password_users" required></input>
                 </div>
                 <div class="mb-3">
-                    <input type="password" id="confMotdepasse" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Confirmez votre mot de passe" name="conf_password" required></input>
+                    <input type="password" id="confMotdepasse" class="bg-antiflashWhite border border-2 border-cardinal text-spaceCadet text-sm rounded-lg block w-full p-2.5" placeholder="Confirmez votre mot de passe" name="conf_password_users" required></input>
                 </div>
             </div>
             <button type="submit" class="text-antiflashWhite mb-3 border border-antiflashWhite border-2 rounded-lg bg-cardinal text-sm w-full sm:w-auto px-5 py-2.5 text-center uppercase">S'inscrire</button>
