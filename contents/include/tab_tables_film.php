9f4870dc982f4c1d1fd53e56acd7e57d76d00ddc
HakuDeBang <mielotr.dev@gmail.com>
2023-03-27 21:26:03 +0200

    table film

--- contents/include/tab_tables_film.php
+++ contents/include/tab_tables_film.php
@@ -4,10 +4,10 @@
         <button class="absolute inset-y-0 left-0 flex items-center pl-3">
             <svg class="w-5 h-5 text-antiflashWHite" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
         </button>
-        <input type="text" id="table-search-users" class="block p-2 pl-10 md:text-md text-antiflashWhite border border-cardinal rounded-lg w-auto bg-spaceCadet focus:ring-cardinal focus:border-cardinal" placeholder="Rechercher un film">
+        <input type="text" id="table-search-films" class="block p-2 pl-10 md:text-md text-antiflashWhite border border-cardinal rounded-lg w-auto bg-spaceCadet focus:ring-cardinal focus:border-cardinal" placeholder="Rechercher un film">
     </div>
     <a href="#" type="button" class="text-antiflashWhite bg-eerieBlack border border-antiflashWhite font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center" data-modal-target="addFilmModal" data-modal-show="addFilmModal">Ajouter un film</a>
-    <?php include('add_film_modal.php'); ?>
+    <?php include 'add_film_modal.php'; ?>
 </form>
 <div class="relative overflow-x-auto shadow-md rounded-lg">
     <table class="w-full text-sm text-center text-antiflashWhite">
@@ -167,7 +167,7 @@
     </table>
     <?php include('edit_film_modal.php'); ?>
 </div>
-<nav class="flex flex-col items-center justify-between pt-4" aria-label="Table navigation">
+<nav class="flex flex-col items-center justify-between pt-4 mb-4" aria-label="Table navigation">
     <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Showing <span class="font-semibold text-gray-900 dark:text-white">1-10</span> of <span class="font-semibold text-gray-900 dark:text-white">1000</span></span>
     <ul class="inline-flex items-center -space-x-px">
         <li>
