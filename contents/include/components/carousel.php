4b93c62ef7e462d4b25173dca2fd21b8031b0584
HakuDeBang <mielotr.dev@gmail.com>
2023-04-04 01:00:02 +0200

    last commit

--- contents/include/components/carousel.php
+++ contents/include/components/carousel.php
@@ -5,9 +5,9 @@
 <div id="default-carousel" class="relative w-full" data-carousel="slide">
     <!-- Carousel wrapper -->
     <div class="relative overflow-hidden rounded-xl sm:mx-14 h-60 sm:h-96 md:h-112 lg:h-144 xl:h-168 my-10">
-        <?php foreach($afficherGalerie as $galerie){ ?>
+        <?php foreach($afficherImages as $images){ ?>
         <div class="hidden duration-3000 ease-in-out" data-carousel-item>
-            <img src="<?= $galerie['lien_galerie']; ?>" class="absolute block w-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            <img src="<?= $images['lien_images']; ?>" class="absolute block w-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
         </div>
         <?php } ?>
     </div>
--- contents/include/components/dispo.php
+++ contents/include/components/dispo.php
@@ -1,3 +1,7 @@
+<?php
+    include '../../../filmopolis/contents/config/admin/pdo.php';
+    include '../../../filmopolis/contents/config/trait_select_films.php';
+?>
 <!-- BANNER DISPO -->
 <div class="flex flex-col items-center justify-center my-10">
     <h2 class="text-antiflashWhite bg-cardinal font-medium rounded-lg text-md px-10 py-1 text-center">Déjà disponible</h2>
@@ -5,78 +9,13 @@
 
 <!-- GRID CARD -->
 <div class="card-group place-items-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 mx-10 mb-10">
-    <!-- ITEM 1 -->
+    <?php foreach($afficherFilm as $film){ ?>
     <div class="w-42 sm:w-44 md:w-54">
         <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
+            <img class="rounded-lg" src="<?= $film['lien_affiche']; ?>" alt="" />
         </a>
     </div>
-    <!-- ITEM 2 -->    
-    <div class="w-42 sm:w-44 md:w-54">
-        <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
-        </a>
-    </div>
-    <!-- ITEM 3 -->    
-    <div class="w-42 sm:w-44 md:w-54">
-        <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
-        </a>
-    </div>
-    <!-- ITEM 4 -->    
-    <div class="w-42 sm:w-44 md:w-54">
-        <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
-        </a>
-    </div>
-    <!-- ITEM 5 -->    
-    <div class="w-42 sm:w-44 md:w-54">
-        <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
-        </a>
-    </div>
-    <!-- ITEM 6 -->    
-    <div class="w-42 sm:w-44 md:w-54">
-        <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
-        </a>
-    </div>
-    <!-- ITEM 7 -->
-    <div class="w-42 sm:w-44 md:w-54">
-        <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
-        </a>
-    </div>
-    <!-- ITEM 8 -->    
-    <div class="w-42 sm:w-44 md:w-54">
-        <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
-        </a>
-    </div>
-    <!-- ITEM 9 -->    
-    <div class="w-42 sm:w-44 md:w-54">
-        <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
-        </a>
-    </div>
-    <!-- ITEM 10 -->    
-    <div class="w-42 sm:w-44 md:w-54">
-        <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
-        </a>
-    </div>
-    <!-- ITEM 11 -->    
-    <div class="w-42 sm:w-44 md:w-54">
-        <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
-        </a>
-    </div>
-    <!-- ITEM 12 -->    
-    <div class="w-42 sm:w-44 md:w-54">
-        <a href="#">
-            <img class="rounded-lg" src="../../../filmopolis/assets/img/pics/filmopolis.png" alt="" />
-        </a>
-    </div>
+    <?php } ?>
 </div>
 
 <!-- PAGINATION DISPO -->
